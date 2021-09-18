1d2604ac64c8accb2982f5fbb6c46a34e8085a42
Jan Xavier Virgen <54022069+janxavierv17@users.noreply.github.com>
2021-09-18 20:35:15 +1000

    Package json.

--- api/app.js
+++ api/app.js
@@ -62,7 +62,6 @@ app.use((err, req, res, next) => {
 
 // set our port
 app.set('port', process.env.PORT || 5000);
-
 // start listening on our port
 const server = app.listen(app.get('port'), () => {
   console.log(`Express server is listening on port ${server.address().port}`);
